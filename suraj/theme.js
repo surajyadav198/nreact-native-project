80d0cb8b7c44465676214e56a983938ce285f216
surajyadav198 <65114895+surajyadav198@users.noreply.github.com>
2024-01-10 17:28:20 +0530

    Update theme.js

--- suraj/theme.js
+++ suraj/theme.js
@@ -10,68 +10,63 @@ export const theme = {
     error: '#f13a59',
   },
 }
-
 import * as React from 'react';
-import { Button, View, Text,StyleSheet,TouchableOpacity,Fl } from 'react-native';
+import { Button, View, Text,StyleSheet,TouchableOpacity,ScrollView, } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
- const data = [
-    {key: 'credit-card', title:'Credit Cards'},
-    {key: 'star', title:'Reward points'},
-    {key: 'heart', title:'Festive Offers'},
-    {key: 'money', title:'Convert to EMI'},
-  ]
+
 
 function HomeScreen({ navigation }) {
 
-    const renderItem = {{ item }} => (
-      <TouchableOpacity></TouchableOpacity>
-    );
-
+    
   return (
+    
     <View >
     <View style={styles.view1}>
       <Text >Total Account Balance</Text>
       <Text style={{fontWeight: 'bold', fontSize: 20}}>N.A</Text>
-     </View>
+    </View>
      <View style={styles.view2}>
       <Text>Keep track of all your bank {'\n'}Account at one place</Text>
       </View>
       <Text style={styles.qlink}>QUICK LINKS</Text>
 
+      <ScrollView horizontal showHorizontalScrollIndicator={false}>
       <View style={styles.view3}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
       <View style={{alignItems:'center', paddingRight:5}}>
         <Icon name="credit-card" size={25} color="red" />
         <Text>Credit Cards</Text>
       </View >
       </TouchableOpacity>
       
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
       <View style={{alignItems:'center', paddingRight:5}}>
         <Icon name="star" size={25} color="red" />
         <Text>Reward Points</Text>
       </View>
       </TouchableOpacity>
       
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Details')}>
       <View style={{alignItems:'center', paddingRight:5}}>
         <Icon name="heart" size={25} color="red" />
         <Text>Festive Offers</Text>
       </View>
       </TouchableOpacity>
       
-       <TouchableOpacity>
+       <TouchableOpacity onPress={() => navigation.navigate('Details')}>
       <View style={{alignItems:'center'}}>
         <Icon name="money" size={25} color="red" />
         <Text>Convert to EMI</Text>
       </View>
       </TouchableOpacity>
       </View>
-
+      
+  </ScrollView>
     </View>
+    
   );
 }
 
@@ -93,7 +88,15 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          option={{
+                        headerLeft: () => {
+              <Text style={{color: 'blue'}}>Logout</Text>
+            },
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -127,6 +130,7 @@ qlink: {
 color: 'black',
 marginTop:10,
 marginLeft:10,
+marginRight:10,
 paddingBottom:5,
 fontWeight:'bold',
 fontSize:15,
